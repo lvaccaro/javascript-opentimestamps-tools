4828faea22ade66d115d0026f49d8e31f40c143c
Luca Vaccaro <luck87@gmail.com>
2018-03-22 14:57:11 +0100

    Update lib

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # javascript-opentimestamp-tools
-JS Tools for javascript-opentimestamps: https://github.com/opentimestamps/javascript-opentimestamps
+JS Tools for [javascript-opentimestamps](https://github.com/opentimestamps/javascript-opentimestamps)
+Test live tools [here](https://opentimestamps.org/tools/)
 
 ## Tools
-* Generate hash (sha1, sha256, ripemd160) from hex. file
+* Generate hash (sha1, sha256, ripemd160) from hex or file
 * stamp the hash & print the ots-proof
 * print info of ots
 * upgrade ots
 * verify ots
 
-## Install dependencies
-To install the dependecies:
+## Get `opentimestamps.js`
+* download from website [opentimestamps.org](https://opentimestamps.org)
+```html
+<script src="https://opentimestamps.org/assets/javascripts/vendor/opentimestamps.js"></script>
 ```
-$ bower install
+* build sources from [javascript-opentimestamps](https://github.com/opentimestamps/javascript-opentimestamps) 
+```bash
+git clone https://github.com/opentimestamps/javascript-opentimestamps.git
+npm install --dev
+npm run dist
 ```
-
-## Run
-Open index.html in your browser.
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const OpenTimestamps = require('javascript-opentimestamps');
+const OpenTimestamps = window.OpenTimestamps;
 
 
 $("#btn-generate").click(function(event){
@@ -168,71 +168,3 @@ $("#btn-verify").click(function(event){
     });
     return false;
 });
-
-function string2Bin(str) {
-    var result = [];
-    for (var i = 0; i < str.length; i++) {
-        result.push(str.charCodeAt(i));
-    }
-    return result;
-}
-function bin2String(array) {
-    return String.fromCharCode.apply(String, array);
-}
-
-function ascii2hex(str) {
-    var arr = [];
-    for (var i = 0, l = str.length; i < l; i ++) {
-        var hex = Number(str.charCodeAt(i)).toString(16);
-        if (hex<0x10) {
-            arr.push("0" + hex);
-        } else {
-            arr.push(hex);
-        }
-    }
-    return arr.join('');
-}
-
-function hex2ascii(hexx) {
-    var hex = hexx.toString();//force conversion
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
-
-function bytesToHex (bytes) {
-    const hex = [];
-    for (var i = 0; i < bytes.length; i++) {
-        hex.push((bytes[i] >>> 4).toString(16));
-        hex.push((bytes[i] & 0xF).toString(16));
-    }
-    return hex.join('');
-};
-
-function hexToBytes(hex) {
-    const bytes = [];
-    for (var c = 0; c < hex.length; c += 2) {
-        bytes.push(parseInt(hex.substr(c, 2), 16));
-    }
-    return bytes;
-};
-
-function stringToArrayBuffer(str){
-    if(/[\u0080-\uffff]/.test(str)){
-        throw new Error("this needs encoding, like UTF-8");
-    }
-    var arr = new Uint8Array(str.length);
-    for(var i=str.length; i--; )
-        arr[i] = str.charCodeAt(i);
-    return arr.buffer;
-}
-
-function arrayBufferToString(buffer){
-    var arr = new Uint8Array(buffer);
-    var str = String.fromCharCode.apply(String, arr);
-    /*if(/[\u0080-\uffff]/.test(str)){
-        throw new Error("this string seems to contain (still encoded) multibytes");
-    }*/
-    return str;
-}
